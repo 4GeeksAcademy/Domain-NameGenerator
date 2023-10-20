de4e204d4f86ea9930fb8ef17f0bc83f150bac2d
JoseR4Geeks <143768985+JoseR4Geeks@users.noreply.github.com>
2023-10-20 14:11:10 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,58 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+let pronounsArray = [];
+let adjectivesArray = [];
+let nounsArray = [];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function addInput() {
+  const pronounsInput = document.querySelector("#pronouns").value;
+  const adjectivesInput = document.querySelector("#adjectives").value;
+  const nounsInput = document.querySelector("#nouns").value;
+
+  pronounsArray.push(
+    ...pronounsInput.split(",").map(pronoun => pronoun.trim())
+  );
+  adjectivesArray.push(
+    ...adjectivesInput.split(",").map(adjective => adjective.trim())
+  );
+  nounsArray.push(...nounsInput.split(",").map(noun => noun.trim()));
+
+  displayWords("added-words-pronoun", pronounsArray);
+  displayWords("added-words-adjective", adjectivesArray);
+  displayWords("added-words-noun", nounsArray);
+
+  document.querySelector("#pronouns").value = "";
+  document.querySelector("#adjectives").value = "";
+  document.querySelector("#nouns").value = "";
+}
+
+function displayWords(listId, wordsArray) {
+  const addedWordsList = document.querySelector(`#${listId}`);
+  addedWordsList.innerHTML = "";
+  wordsArray.forEach(word => {
+    addedWordsList.innerHTML += `<li>${word}</li>`;
+  });
+}
+
+function generateDomainCombinations() {
+  const domainCombinations = [];
+
+  pronounsArray.forEach(pronoun => {
+    adjectivesArray.forEach(adjective => {
+      nounsArray.forEach(noun => {
+        const domainName = `${pronoun}${adjective}${noun}.com`;
+        domainCombinations.push(domainName);
+      });
+    });
+  });
+
+  document.querySelector(
+    "#domain-combinations"
+  ).innerHTML = domainCombinations.join("<br>");
+}
+
+document.querySelector("#addInput").addEventListener("click", addInput);
+document
+  .querySelector("#generateDomains")
+  .addEventListener("click", generateDomainCombinations);
